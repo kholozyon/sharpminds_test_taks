87ebefaeec2039c14d3799e15ef38e38af07720c
Anatoly Kholozyon <kholozyon@gmail.com>
2018-08-12 22:45:43 +0300

    unit tests

--- src/modules/App.test.js
+++ src/modules/App.test.js
@@ -1,9 +1,34 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import { shallow, configure } from 'enzyme';
+import { shallowToJson } from 'enzyme-to-json';
+import Adapter from 'enzyme-adapter-react-16';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+
+configure({ adapter: new Adapter() });
+
+describe('Tests:', function() {
+
+  it('renders Tabs name', () => {
+    const tabHeader = shallow(<li className="tabs__list-item"></li>);
+    expect(tabHeader).toHaveLength(1);
+  });
+
+  it('renders active Tab', () => {
+    const tabHeader = shallow(<li className="active"></li>);
+    expect(tabHeader).toHaveLength(1);
+  });
+
+  it('renders Tabs content', () => {
+    const tabContent = shallow(<div className="tabs__content"></div>);
+    expect(tabContent).toHaveLength(1);
+  });
+
+  it('Test click event', () => {
+    const mockCallBack = jest.fn();
+
+    const button = shallow((<li onClick={mockCallBack}></li>));
+    button.find('li').simulate('click');
+    expect(mockCallBack.mock.calls.length).toEqual(1);
+  });
 });
+
